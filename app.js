0c09611b8d05c1f307394ada44b0f62b42a3d661
ukaran11 <ukaran11@gmail.com>
2021-09-08 20:16:09 +0530

    Utsav | Added the inital get tours route

---
+++ app.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours: tours
+        }
+    })
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
