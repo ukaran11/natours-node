e367aa7acb00916306f6afc48d2e261f131ff16a
ukaran11 <ukaran11@gmail.com>
2021-09-09 16:45:18 +0530

    Utsav | Refactored server init into its own server.js

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// START SERVER
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
+// START SERVER
+
