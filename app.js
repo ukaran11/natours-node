e4a98d7893e90d19acde7f6f322a8065577a2be5
ukaran11 <ukaran11@gmail.com>
2021-09-10 03:22:33 +0530

    Utsav | Configured the env settings

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // MIDDLEWARES
-app.use(morgan("dev"));
+if(process.env.NODE_ENV ==='development'){
+  app.use(morgan("dev"));
+}
+  
 app.use(express.json());
+// app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
