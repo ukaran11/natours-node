8a410970485c5f5e3d3fff472e1635e379f9e0c7
ukaran11 <ukaran11@gmail.com>
2021-09-11 02:58:42 +0530

    Utsav | configured env

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // MIDDLEWARES
-if(process.env.NODE_ENV ==='development'){
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-  
+
 app.use(express.json());
 // app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
-})
+});
 
 // ROUTE HANDLERS
 
@@ -28,11 +27,8 @@ app.use((req, res, next) => {
 // app.patch("/api/v1/tours/:id", updateTour);
 // app.delete("/api/v1/tours/:id", deleteTour);
 
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-
 module.exports = app;
 // START SERVER
-
