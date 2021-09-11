6de2df64d15285d77a45a30505c9735831765a56
ukaran11 <ukaran11@gmail.com>
2021-09-11 16:42:36 +0530

    Utsav | Added support for delete tour operation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,7 +23,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
-    res.status(200).json({
+    res.status(204).json({
       status: 'success',
       data: {
         tour,
@@ -78,11 +78,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: {
-      tour: 'Tour Deleted ...',
-    },
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
